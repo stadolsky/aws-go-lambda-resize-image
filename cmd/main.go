74bc0d35da9b79df6db23e4de665d05e805481b2
Roman <stadolsky@gmail.com>
2021-02-14 01:41:10 +0200

    fix go mod version 1 done

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,118 @@ package main
 
 import (
 	"bytes"
-	"encoding/base64"
 	"fmt"
+	"io/ioutil"
+
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"gopkg.in/gographics/imagick.v2/imagick"
+)
+
+type Event struct {
+	Region      string      `json:"region"`
+	InBucket    string      `json:"in_bucket"`
+	InImageKey  string      `json:"in_image_key"`
+	OutBucket   string      `json:"out_bucket"`
+	OutImageKey string      `json:"out_image_key"`
+	Resolution  string      `json:"resolution"`
+	OutFormat   imageFormat `json:"out_format"`
+}
+
+type imageFormat string
+
+const (
+	imageFormatJPG imageFormat = "jpg"
+	imageFormatPNG imageFormat = "png"
 )
 
 func main() {
 	lambda.Start(HandleLambdaEvent)
 }
 
-type Event struct {
-	InBucket    string `json:"in_bucket"`
-	InImageKey  string `json:"in_image_key"`
-	OutBucket   string `json:"out_bucket"`
-	OutImageKey string `json:"out_image_key"`
-	Resolution  string `json:"resolution"`
+func HandleLambdaEvent(event Event) error {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(event.Region)})
+	if err != nil {
+		return fmt.Errorf("failed to create NewSession for SDK: %w", err)
+	}
+
+	svc := s3.New(sess)
+
+	input := s3.GetObjectInput{
+		Bucket: aws.String(event.InBucket),
+		Key:    aws.String(event.InImageKey),
+	}
+
+	objectOutput, err := svc.GetObject(&input)
+	if err != nil {
+		return fmt.Errorf("failed to get object `%s` in bucket `%s`: %w", event.InImageKey, event.InBucket, err)
+	}
+
+	defer objectOutput.Body.Close()
+
+	// Read the chunk
+	originalImageData, err := ioutil.ReadAll(objectOutput.Body)
+	newImageData, err := resize(originalImageData, event.OutFormat)
+	if err != nil {
+		return fmt.Errorf("fialde to create new resided image: %w", err)
+	}
+
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Body:   bytes.NewReader(newImageData),
+		Bucket: aws.String(event.OutBucket),
+		Key:    aws.String(event.OutImageKey),
+	})
+
+	fmt.Println(fmt.Sprint("New file size: %d", len(newImageData)))
+
+	if err != nil {
+		return fmt.Errorf("failed to save image with keyobject `%s` in bucket `%s`: %w", event.OutImageKey, event.OutBucket, err)
+	}
+
+	return nil
 }
 
-type Response struct {
-	PDF64 string `json:"pdf"`
-}
+func resize(data []byte, format imageFormat) ([]byte, error) {
+	imagick.Initialize()
+	// Schedule cleanup
+	defer imagick.Terminate()
+	var err error
 
-func HandleLambdaEvent(event Event) (*Response, error) {
+	mw := imagick.NewMagickWand()
 
-	return nil, nil
+	err = mw.ReadImageBlob(data)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get original logo size
+	width := mw.GetImageWidth()
+	height := mw.GetImageHeight()
+
+	// Calculate half the size
+	hWidth := uint(width / 2)
+	hHeight := uint(height / 2)
+
+	// Resize the image using the Lanczos filter
+	// The blur factor is a float, where > 1 is blurry, < 1 is sharp
+	err = mw.ResizeImage(hWidth, hHeight, imagick.FILTER_LANCZOS, 1)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the compression quality to 95 (high quality = low compression)
+	err = mw.SetImageCompressionQuality(95)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert into JPG
+	if err := mw.SetFormat(string(format)); err != nil {
+		return nil, err
+	}
+
+	return mw.GetImageBlob(), nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module aws-go-lambda-resize-image
 
 go 1.15
 
-require github.com/aws/aws-lambda-go v1.19.1
+require (
+	github.com/aws/aws-lambda-go v1.19.1
+	github.com/aws/aws-sdk-go v1.37.10
+	github.com/kr/pretty v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/gographics/imagick.v2 v2.6.0
+)
